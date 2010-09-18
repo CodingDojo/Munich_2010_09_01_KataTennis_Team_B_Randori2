d57b89439a6e303a7d8707ef2ed18b27cb5e4aad
Christian Deger <christian@deger.eu>
2010-09-18 13:47:51 +0200

    the only commit

---
+++ TennisGame/TennisGameTests/TennisGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TennisGameTests
+{
+    public class TennisGame
+    {
+        private Dictionary<TennisPlayer, int> scores = new Dictionary<TennisPlayer, int>()
+            {
+                { TennisPlayer.One, 0 },
+                { TennisPlayer.Two, 0 }
+            };
+
+        public bool IsOver 
+        {
+            get
+            {
+                return 
+                    PlayersHaveTwoPointsDifference() && 
+                    AnyPlayerHasAtMoreThenPoints();
+            }
+        }
+
+        public void Score(TennisPlayer tennisPlayer)
+        {
+            scores[tennisPlayer]++;
+        }
+
+        private bool PlayersHaveTwoPointsDifference()
+        {
+            return Math.Abs(scores[TennisPlayer.One] - scores[TennisPlayer.Two]) > 1;
+        }
+
+        private bool AnyPlayerHasAtMoreThenPoints()
+        {
+            return scores[TennisPlayer.One] > 3 || scores[TennisPlayer.Two] > 3;
+        }
+    }
+}
---
+++ TennisGame/TennisGameTests/TennisGameTest.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGameTests
+{
+    [TestClass]
+    public class TennisGameTest
+    {
+        [TestMethod]
+        public void When_Both_Players_Have_No_Points_Then_Game_Is_Not_Over()
+        {
+            TennisGame game = new TennisGame();
+            Assert.IsFalse(game.IsOver);
+        }
+
+        [TestMethod]
+        public void When_Player_One_Scores_And_Player_Two_Has_One_Point_Less_Then_Game_Is_Over()
+        {
+            TennisGame game = new TennisGame();
+
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two); // 30
+
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One); // game
+
+            Assert.IsTrue(game.IsOver);
+        }
+
+        [TestMethod]
+        public void When_Both_Players_Have_Same_Score_And_Player_One_Scores_Then_Game_Is_Not_Over()
+        {
+            TennisGame game = new TennisGame();
+
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two); // 40
+
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One); // adv
+
+            Assert.IsFalse(game.IsOver);
+        }
+
+        [TestMethod]
+        public void When_Both_Players_Have_40_Score_And_Player_One_Scores_Two_Times_Then_Game_Is_Not_Over()
+        {
+            TennisGame game = new TennisGame();
+
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two); // 40
+
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One); // adv
+            game.Score(TennisPlayer.One); // game
+
+            Assert.IsTrue(game.IsOver);
+        }
+
+        [TestMethod]
+        public void When_Both_Players_Have_40_Score_And_Player_Two_Scores_Two_Times_Then_Game_Is_Not_Over()
+        {
+            TennisGame game = new TennisGame();
+
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One); //40
+
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two); // 40
+            game.Score(TennisPlayer.Two); // adv
+            game.Score(TennisPlayer.Two); // game
+
+            Assert.IsTrue(game.IsOver);
+        }
+
+        [TestMethod]
+        public void When_Player_Two_Has_Advantage_And_Player_One_Scores_Three_Times_Game_Is_Over()
+        {
+            TennisGame game = new TennisGame();
+
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two); // 40
+            game.Score(TennisPlayer.Two); // adv
+
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One); //40
+
+            game.Score(TennisPlayer.One); //deuce
+            game.Score(TennisPlayer.One); //adv 
+            game.Score(TennisPlayer.One); //game 
+
+            Assert.IsTrue(game.IsOver);
+        }
+
+        [TestMethod]
+        public void When_Player_Two_Has_Advantage_And_Player_One_Scores_One_Time_Game_Is_Not_Over()
+        {
+            TennisGame game = new TennisGame();
+
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two);
+            game.Score(TennisPlayer.Two); // 40
+            game.Score(TennisPlayer.Two); // adv
+
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One); //40
+            game.Score(TennisPlayer.One); 
+
+            Assert.IsFalse(game.IsOver);
+        }
+
+        [TestMethod]
+        public void When_Player_One_Scores_Two_Times_And_Player_Two_Does_Not_Score_Then_Game_Is_Not_Over()
+        {
+            TennisGame game = new TennisGame();
+
+            game.Score(TennisPlayer.One);
+            game.Score(TennisPlayer.One);
+
+            Assert.IsFalse(game.IsOver);
+        }
+    }
+}
